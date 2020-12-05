c5edf5271a3f6d81deb1f0369f498600f02de8ec
Abhiram <74865607+abhi98ram@users.noreply.github.com>
2020-12-05 14:31:06 +0530

    docs:added

---
+++ app.js
@@ -0,0 +1,23 @@
+var ServerURL = '	https://api.funtranslations.com/translate/pig-latin.json';
+var inputText = document.querySelector('#txt-input');
+var  btnTranslate = document.querySelector('#btn-translate');
+var outputText = document.querySelector('#txt-output');
+
+function constructURL(fetchedText) {
+    return ServerURL +'?'+'text='+fetchedText
+}
+
+function errorHandler(error) {
+    alert('Error occured' + error);
+}
+function clickEventHandler() {
+    let text = inputText.value;
+    fetch(constructURL(text))
+        .then(response => response.json())
+        .then( json => {
+            let data = json.contents.translated
+            outputText.innerHTML = data;
+        })
+    .catch(errorHandler)
+}
+btnTranslate.addEventListener('click', clickEventHandler);
